dbb97095a483d334ce0750b47fd4b07dc02118e0
srinadhravipati <ravipatisrinadh111@gmail.com>
2025-10-15 13:12:37 -0400

    initial commit

---
+++ unitPratice/src/main/java/galmart/unitPratice/UnitPraticeApplication.java
@@ -0,0 +1,13 @@
+package galmart.unitPratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitPraticeApplication.class, args);
+	}
+
+}
